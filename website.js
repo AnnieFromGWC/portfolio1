e5fbacf1b551844bfc52795e2c46d63fd223f7b5
AnnieFromGWC <41089344+AnnieFromGWC@users.noreply.github.com>
2018-07-10 15:37:03 -0700

    Create website.js

---
+++ website.js
@@ -0,0 +1,10 @@
+function showpic() {
+  var x = document.getElementById("myPic");
+  // x.style.display = "block";
+
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
